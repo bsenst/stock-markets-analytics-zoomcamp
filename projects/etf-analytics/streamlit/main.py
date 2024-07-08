9abe8c5cd7a497b27b69898548a7559e61574a6c
Benjamin Senst <benjamin.senst@gmail.com>
2024-07-08 22:37:08 +0200

    change readme path

--- projects/etf-analytics/streamlit/main.py
+++ projects/etf-analytics/streamlit/main.py
@@ -20,6 +20,6 @@ from pathlib import Path
 def read_markdown_file(markdown_file):
     return Path(markdown_file).read_text()
 
-intro_markdown = read_markdown_file("README.md")
+intro_markdown = read_markdown_file("projects/etf-analytics/README.md")
 
 st.markdown(intro_markdown, unsafe_allow_html=True)
